0841bcb58818f73393acfce5b8c866eac98ced42
mlm162 <makayla.kemp@my.utsa.edu>
2026-04-15 16:15:34 -0500

    Add fields and methods to Player class

--- Player.java
+++ Player.java
@@ -1,3 +1,13 @@
 
 public class Player {
+  String name;
+  ArrayList<Card> hand;
+  boolean isBot;
+
+  public void playCard(){
+  }
+  public void drawCard(){
+  }
+  public boolean hasValidMove(){
+  }
 }
