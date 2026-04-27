0a127472336620cd71dff40e0b3839a3ee766739
mlm162 <makayla.kemp@my.utsa.edu>
2026-04-27 16:27:28 -0500

    UpdateSetting

--- src/main/java/controller/SettingsController.java
+++ src/main/java/controller/SettingsController.java
@@ -6,11 +6,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+public String filePath = "settings.csv";
 public class SettingsController{
+    @fxml private ComboBox<String> launguageBox;
+    @fxml private ComboBox<String> controlsBox;
+    @fxml private ComboBox<String> appearanceBox;
+    @fxml private ComboBox<String> resolutionBox;
+    @fxml private ComboBox<String> displayModeBox;
+
+    @fxml private CheckBox musicCheck;
+    @fxml private CheckBox sfxCheck;
+    @fxml private CheckBox colorBlindCheck;
     public void initailize(){
         /* Calls the method that loads settings from the CSV
         Populates all UI elements (ComboBoxes + CheckBoxes) using the loaded data
         Ensures the UI reflects the current saved values */
+        
+        loadSettings();
+        setupComboBox(languageBox, "language");
+        setupComboBox(controlsBox, "controls");
+        setupComboBox(appearanceBox, "appearance");
+        setupComboBox(resolutionBox, "resolution");
+        setupComboBox(displayModeBox, "displayMode");
+
+        setupCheckBox(musicCheck, "music");
+        setupCheckBox(sfxCheck, "SoundFX");
+        setupCheckBox(colorBlindCheck, "colorBlind");
 
     }
     public void loadSettings(){
@@ -18,8 +40,20 @@ public class SettingsController{
         Skips the header row
         Parses each line into a structured setting object
         Stores all settings in a collection (like a map using keys such as "language", "music", etc.) */
-        String filePath = "settings.csv";
-        
+        try (BufferedReader settings = new BufferedReader("settings.csv")){
+            String file;
+            settings.readLine();
+
+            while(file = settings.readLine() != null){
+                string[] column = line.split(",");
+            }
+
+
+
+        } catch (IOException e){
+
+        }
+
     }
     private void setupComboBox(ComboBox<String> box, String settingKey){
         /* Takes a ComboBox and a setting key (like "language")
@@ -37,8 +71,8 @@ public class SettingsController{
         Updates the in-memory settings data
         Calls a method to persist (write) everything back to the CSV */
     }
-    public void saveToCSV(){
-        String filePath = "settings.csv";
+    public void saveToCSV(String filePath){
+        
         /* Writes all settings back into settings.csv
         Preserves the correct format:
         Setting name
@@ -46,6 +80,11 @@ public class SettingsController{
         Current value
         Options
         Overwrites or updates the file with new value */
+        try {
+
+        } catch(IOException e){
+
+        }
     }
     public void handleBack(){
         /*Triggered when the user clicks Back
