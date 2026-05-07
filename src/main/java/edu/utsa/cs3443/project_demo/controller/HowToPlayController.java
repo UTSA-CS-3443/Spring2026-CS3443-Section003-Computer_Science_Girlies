29581a8eaea14f12e3ba36302b6b9cde6ccab2a4
emoran2004 <elisamoran2004@gmail.com>
2026-05-06 22:12:36 -0500

    Updated HowToPlayController.java

--- src/main/java/edu/utsa/cs3443/project_demo/controller/HowToPlayController.java
+++ src/main/java/edu/utsa/cs3443/project_demo/controller/HowToPlayController.java
@@ -2,12 +2,14 @@ package edu.utsa.cs3443.project_demo.controller;
 
 import java.io.IOException;
 
+import edu.utsa.cs3443.project_demo.MainApp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class HowToPlayController {
@@ -23,13 +25,36 @@ public class HowToPlayController {
 
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-            Scene scene = new Scene(root, 900, 600);
+            StackPane wrapper = new StackPane(root);
+            wrapper.setStyle("-fx-background-color: #9e9e9e;");
+
+            Scene scene = new Scene(wrapper, stage.getWidth(), stage.getHeight());
 
             stage.setScene(scene);
-            stage.setResizable(false);
-            stage.setMaximized(false);
-            stage.setWidth(900);
-            stage.setHeight(600);
+
+            if (MainApp.currentDisplayMode.equalsIgnoreCase("Fullscreen")) {
+                stage.setFullScreen(true);
+                stage.setMaximized(false);
+
+            } else if (MainApp.currentDisplayMode.equalsIgnoreCase("Maximized")) {
+                stage.setFullScreen(false);
+                stage.setMaximized(true);
+
+            } else {
+                stage.setFullScreen(false);
+                stage.setMaximized(false);
+
+                if (MainApp.currentResolution.contains("x")) {
+                    String[] parts = MainApp.currentResolution.toLowerCase().split("x");
+
+                    double width = Double.parseDouble(parts[0].trim());
+                    double height = Double.parseDouble(parts[1].trim());
+
+                    stage.setWidth(width);
+                    stage.setHeight(height);
+                }
+            }
+
             stage.centerOnScreen();
 
         } catch (IOException e) {
